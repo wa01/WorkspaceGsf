4640a99dd045a37e319ff48925d40075bad83d8b
Wolfgang Adam <wolfgang.adam@cern.ch>
2017-09-06 17:48:39 +0200

    bug fix & more features for vtxTsosDebug

--- GsfTrajectoryAnalyzer/test/vtxTsosDebug.py
+++ GsfTrajectoryAnalyzer/test/vtxTsosDebug.py
@@ -37,9 +37,9 @@ def drawMixture(pad,title,mgs,xmin=None,xmax=None,superimpose=False):
     pad.SetGridy(1)
     pad.Update()
 
-    line = "Mean : {0:10.2e}#pm{1:9.2e}  Mode : {0:10.2e}".format(mgs.mean(),mgs.sigma(),mode[0])
+    line = "Mean : {0:10.2e}#pm{1:9.2e}  Mode : {2:10.2e}".format(mgs.mean(),mgs.sigma(),mode[0])
     if superimpose:
-        latex = ROOT.TLatex(0.1,0.90,line)
+        latex = ROOT.TLatex(0.1,0.91,line)
         latex.SetTextColor(2)
     else:
         latex = ROOT.TLatex(0.1,0.95,line)
@@ -53,11 +53,14 @@ def drawMixture(pad,title,mgs,xmin=None,xmax=None,superimpose=False):
 
 class DrawSingleTrack:
 
-    def __init__(self,fname,index=None,otherCanvas=None):
+    def __init__(self,fname,index=None,otherCanvas=None,event=(None,None,None)):
         self.fname = fname
         self.index = index
         self.otherCanvas = otherCanvas
+        self.canvasTitle = None
 
+        self.event = event
+        self.runLumiEvt = None
         self.itrack = 0
         self.canvas = None
         self.icnv = 0
@@ -81,7 +84,21 @@ class DrawSingleTrack:
         self.vtxMode = None
         self.coords = [ ]
         self.ival = None
+        showEvent = True
         for il,l in enumerate(open(self.fname)):
+
+            if self.icnv==0:
+                px, py, pz = None, None, None
+
+            if self.otherCanvas==None and l.startswith("Begin processing"):
+                fields = l[:-1].split()
+                if fields[5]=="Run" and fields[7]=="Event" and fields[9]=="LumiSection":
+                    self.canvasTitle = "Run "+fields[6][:-1]+" Lumi "+fields[10]+" Evt "+fields[8][:-1]
+                    self.runLumiEvt = ( int(fields[6][:-1]), int(fields[10]), int(fields[8][:-1]) )
+                    showEvent = ( self.event[0]==None or self.event[0]==self.runLumiEvt[0] ) and \
+                                ( self.event[1]==None or self.event[1]==self.runLumiEvt[1] ) and \
+                                ( self.event[2]==None or self.event[2]==self.runLumiEvt[2] )
+                continue
 
             if self.vtxMode==None and l.startswith("vtxTSOS mode"):
                 fields = l[:-1].split()
@@ -123,10 +140,19 @@ class DrawSingleTrack:
                     mgs.setStates(wgts,means,sigmas)
                     self.icnv += 1
                     self.canvas.cd(self.icnv)
-                    if self.index==None or self.itrack==self.index:
+                    if ( self.index==None or self.itrack==self.index ) and showEvent:
                         if self.coords[-1] in [ 'px', 'py', 'pz', 'p' ]:
                             drawMixture(ROOT.gPad,self.coords[-1],mgs,xmin=-200.,xmax=200., \
                                         superimpose=(self.otherCanvas!=None))
+                            if self.coords[-1]=='px':
+                                px = mgs.mode()[0]
+#                                print "px = ",px
+                            elif self.coords[-1]=='py':
+                                py = mgs.mode()[0]
+#                                print "py = ",py
+                            elif self.coords[-1]=='pz':
+                                pz = mgs.mode()[0]
+#                                print "pz = ",pz
                         elif self.coords[-1]=='qp':
                             drawMixture(ROOT.gPad,self.coords[-1],mgs,xmin=-1./2.5,xmax=1./2.5, \
                                         superimpose=(self.otherCanvas!=None))
@@ -139,7 +165,14 @@ class DrawSingleTrack:
                         self.vtxMode = None
                         self.coords = [ ]
                 if self.icnv>=9:
-                    if self.index==None or self.itrack==self.index:
+                    if self.canvasTitle!=None:
+                        title = "Track "+str(self.itrack)
+                        if px!=None and py!=None and pz!=None:
+                            p3 = ROOT.TVector3(px,py,pz)
+                            title += " ; Pt {0:4.1f} Eta {1:5.2f}".format(p3.Perp(),p3.Eta())
+                        title += " ; "+self.canvasTitle
+                        self.canvas.SetTitle(title)
+                    if ( self.index==None or self.itrack==self.index ) and showEvent:
                         self.canvas.Update()
                         raw_input("Enter")
                     if self.itrack==self.index:
@@ -157,11 +190,14 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--itrack', '-i', type=int, default=None)
+    parser.add_argument('--run', '-r', type=int, default=None)
+    parser.add_argument('--lumi', '-l', type=int, default=None)
+    parser.add_argument('--evt', '-e', type=int, default=None)
     parser.add_argument('files',nargs='+',default=None)
     args = parser.parse_args()
     assert len(args.files)<=2 and (len(args.files)==1 or args.itrack!=None)
 
-    drawClass = DrawSingleTrack(fname=args.files[0],index=args.itrack)
+    drawClass = DrawSingleTrack(fname=args.files[0],index=args.itrack,event=(args.run,args.lumi,args.evt))
     cnv = drawClass.drawTrack()
 
     if len(args.files)>1:    
